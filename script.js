cf5b5935c9dbbb878462a94f888639e5af68dcef
kiiimng <kimleyeng@gmail.com>
2018-11-19 09:56:42 +1100

    added sounds

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-window.addEventListener('keydown', function (e) {
-  console.log(e)
-})
+
+let drums = document.querySelectorAll(".drum-pad");
+for (let i of drums) {
+	i.addEventListener("click", playSound)
+}
+
+function playSound(e){
+	let padLetter = e.currentTarget.childNodes[1].innerHTML;
+
+	//use text to get element by id -> audio
+	//
+	document.getElementById(padLetter).play()
+}
+
+document.getElementById("display").addEventListener("click", playSound)
