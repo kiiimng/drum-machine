b4d1b365ed5fdc0d2e9887d2652c18552aca5c12
kiiimng <kimleyeng@gmail.com>
2018-11-19 14:31:29 +1100

    new color theme and display sound

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 
 let drums = document.querySelectorAll(".drum-pad");
 for (let i of drums) {
-	i.addEventListener("click", playSound)
+	i.addEventListener("click", playSound);
+	i.addEventListener("transitionend", removeClass);
 }
 
 function playSound(e){
-	let padLetter = e.currentTarget.childNodes[1].innerHTML;
-
-	//use text to get element by id -> audio
-	//
-	document.getElementById(padLetter).play()
+	this.childNodes[3].currentTime = 0;
+	this.childNodes[3].play();
+	this.childNodes[1].classList.add("playing");0
+	document.getElementById("display").innerHTML = this.id
 }
 
-document.getElementById("display").addEventListener("click", playSound)
+
+function keySound(e) {
+	const eventKey = e.key.toUpperCase();
+	const audio = document.getElementById(eventKey)
+	if (!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+	audio.parentNode.childNodes[1].classList.add("playing");
+ }
+
+document.addEventListener('keydown', keySound);
+
+function removeClass(e){
+	if (e.propertyName !== "font-size") return;
+	this.childNodes[1].classList.remove("playing")
+
+}
+
+
+//display sounds in display div
